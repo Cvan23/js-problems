23b3868bff66dafbc0921c3987b1c53b576757de
Chris Van Huffe <cvanhuffel@outlook.com>
2022-01-09 14:16:56 -0800

    removeDup1 updated

--- remove-duplicates/removeDup1.js
+++ remove-duplicates/removeDup1.js
@@ -9,21 +9,14 @@ console.log(arr1);
 
 let point1 = 0;
 
-for(let point2 = 1; point2 < arr1.length; point2++) {
+for(let point2 = 1; point2 < arr1.length -1; point2++) {
   if(arr1[point1] !== arr1[point2]) {
     point1 ++;
     arr1[point1] = arr1[point2];
-    // console.log(point1,point2);
+
     if(arr1[point1] === arr1[point2]) {
-        arr1.slice(); 
+        arr1.splice(point2,1); 
     }
-  }
-  
+  } 
 }
 console.log(arr1);
-
-
-
-
-
-
