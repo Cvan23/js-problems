aac020bcbe9fa078f41dbb1f453efec6108695ad
Chris Van Huffe <cvanhuffel@outlook.com>
2022-01-09 16:00:12 -0800

    removeDup4 added

---
+++ remove-duplicates/removeDup4.js
@@ -0,0 +1,24 @@
+// Weird solution, a work in progress that isn't working.
+
+const arr1 = [1,1,5,4,6,6,6,2,3,2,1,4,3,5];
+
+function numCompare(num1, num2) {
+  return num1 - num2;
+}
+arr1.sort();
+
+console.log(arr1);
+
+let point1 = 0;
+
+for(let point2 = 1; point2 < arr1.length -1; point2++) {
+  if(arr1[point1] !== arr1[point2]) {
+    point1 ++;
+    arr1[point1] = arr1[point2];
+
+    if(arr1[point1] === arr1[point2]) {
+        arr1.splice(point2,1); 
+    }
+  } 
+}
+console.log(arr1);
